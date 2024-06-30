d6b280951a3c5e069298b29d4a97e9cba4a494f4
berno93 <berno.om@gmail.com>
2024-06-30 13:40:47 +0200

    maj

--- multilang_site/settings.py
+++ multilang_site/settings.py
@@ -162,6 +162,7 @@ APPEND_SLASH = True
 #     # BASE_DIR / "main/static",
 #     os.path.join(BASE_DIR, "main/static")
 # ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
